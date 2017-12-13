fdfd7dab0e25e90be0d35e8ec7babfe9be701835
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2017-12-13 15:43:34 +0000

    Replace Flask-Rest

--- oracc_rest/__init__.py
+++ oracc_rest/__init__.py
@@ -1,54 +1,60 @@
-from flask import Flask, request, abort
-from flask_restful import Resource, Api
+from flask import Flask
 
 from .search import ESearch
 
 
 app = Flask(__name__)
-api = Api(app)
 
 
-class SingleFieldSearch(Resource):
-    def get(self):
-        args = request.form
-        # Parse request
-        if not args:
-            abort(400, 'No query specified!')
-        elif len(args) > 1:
-            abort(400, 'Too many fields specified!')
-        fieldname, word = list(args.items())[0]
-        # Pass to ElasticSearch
-        search = ESearch()
-        results = search.run(word, fieldname)
-        # Return search results to caller
-        if not results:
-            return {}, 204  # "empty content" response if no results found
-        return results
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 
-class GeneralSearch(Resource):
-    def get(self, word):
-        """Search "all" fields in the database for the given word."""
-        # Pass to ElasticSearch
-        search = ESearch()
-        results = search.run(word)
-        # Return search results to caller
-        if not results:
-            return {}, 204  # "empty content" response if no results found
-        return results
+@app.route('/search/<word>')
+def general_search(word):
+    """Search "all" fields in the database for the given word."""
+    # Pass to ElasticSearch
+    search = ESearch()
+    results = search.run(word)
+    # Return search results to caller
+    if not results:
+        return ''
+    return str(results)
 
 
-class FullList(Resource):
-    def get(self):
-        """Search "all" fields in the database for the given word."""
-        # Pass to ElasticSearch
-        search = ESearch()
-        results = search.list_all()
-        # Return search results to caller
-        return results
+@app.route('/search_all')
+def all_entries():
+    """Search "all" fields in the database for the given word."""
+    # Pass to ElasticSearch
+    search = ESearch()
+    results = search.list_all()
+    # Return search results to caller
+    return str(results)
+
+
+# class SingleFieldSearch(Resource):
+#     def get(self):
+#         args = request.form
+#         # Parse request
+#         if not args:
+#             abort(400, 'No query specified!')
+#         elif len(args) > 1:
+#             abort(400, 'Too many fields specified!')
+#         fieldname, word = list(args.items())[0]
+#         # Pass to ElasticSearch
+#         search = ESearch()
+#         results = search.run(word, fieldname)
+#         # Return search results to caller
+#         if not results:
+#             return {}, 204  # "empty content" response if no results found
+#         return results
 
 
 # Make the search API available at the "/search" endpoint
-api.add_resource(SingleFieldSearch, '/search')
-api.add_resource(GeneralSearch, '/search/<string:word>')
-api.add_resource(FullList, '/search_all')
+# api.add_resource(SingleFieldSearch, '/search')
+# api.add_resource(GeneralSearch, '/search/<string:word>')
+# api.add_resource(FullList, '/search_all')
