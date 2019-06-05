f053fcc60ec5a7119b2f0f6a236ff44e831746b8
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2019-06-05 11:59:19 +0100

    Separate processing sub-steps into functions
    Mostly for easier testing, and also adding some more detailed
    explanation.

--- ingest/break_down.py
+++ ingest/break_down.py
@@ -52,31 +52,23 @@ def process_entry(entry):
     return new_entry
 
 
-def process_file(input_name, write_file=True):
+def process_glossary_data(data):
     """
-    Process all entries in a glossary file, extracting the common information to
-    create entries that can be individually indexed. Optionally create a new
-    file with the entries that can be uploaded manually.
+    Process a glossary and link the entries to their instances.
 
-    :param input_name: the name of the glossary JSON file
-    :param write_file: whether to write the entries in a new file, to be used later
-    :return: a list of the new individual entries, as dictionaries
+    Glossaries contain entries in a nested format. This step extracts the
+    relevant information at various nesting levels, and produces a list of
+    entries with "flattened" fields. It also incorporates the information from
+    the instances part of the glossary into the relevant entries.
+    Any entries referring to non-existent instances will be ignored. A warning
+    will be raised in those cases.
+
+    :param data: a dictionary representing a glossary, including the instances.
+    :return: a list of entries, flattened and linked to instances when possible.
+
     """
-    # The glossaries contain a lot of information that we do not use.
-    # Sometimes this can make them too large to load in memory. Therefore,
-    # we first preprocess each file to remove the fields we do not need.
-    filter_file = os.path.join("ingest", "remove_unused.jq")
-    s = subprocess.run(
-            ["jq", "-f", filter_file, input_name],
-            stdout=subprocess.PIPE
-    )
-
-    # We need to decode the output to a string if not working in binary mode
-    data = json.loads(s.stdout.decode("utf8"))
-
     instances = data["instances"]
     base_data = {key: data[key] for key in base_fields}
-
     new_entries = []
     for entry in data["entries"]:
         # Create a flat entry from the nested norms, forms, senses etc.
@@ -96,6 +88,36 @@ def process_file(input_name, write_file=True):
         # Add the attributes shared by all entries in the glossary
         new_entry.update(base_data)
         new_entries.append(new_entry)
+    return new_entries
+
+
+def preprocess_glossary(glossary_filename):
+    """Remove unused fields from a glossary and return it as a dictionary."""
+    filter_file = os.path.join("ingest", "remove_unused.jq")
+    s = subprocess.run(
+            ["jq", "-f", filter_file, glossary_filename],
+            stdout=subprocess.PIPE
+    )
+    # We need to decode the output to a string if not working in binary mode
+    return json.loads(s.stdout.decode("utf8"))
+
+
+def process_file(input_name, write_file=True):
+    """
+    Process all entries in a glossary file, extracting the common information to
+    create entries that can be individually indexed. Optionally create a new
+    file with the entries that can be uploaded manually.
+
+    :param input_name: the name of the glossary JSON file
+    :param write_file: whether to write the entries in a new file, to be used later
+    :return: a list of the new individual entries, as dictionaries
+    """
+    # The glossaries contain a lot of information that we do not use.
+    # Sometimes this can make them too large to load in memory. Therefore,
+    # we first preprocess each file to remove the fields we do not need.
+    data = preprocess_glossary(input_name)
+
+    new_entries = process_glossary_data(data)
     if write_file:
         output_name = input_name.rsplit('.', 1)[0] + "-entries.json"
         with open(output_name, 'w') as outfile:
