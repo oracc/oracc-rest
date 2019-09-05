692ae839b87d003e1a432b588f8ddec300079615
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2019-09-05 15:00:03 +0100

    Start moving towards using the higher-level Index
    The Index object exposes some methods of the index client (like
    put_mapping) but gives a higher-level abstraction, which will
    hopefully lead to more concise code.

--- ingest/prepare_index.py
+++ ingest/prepare_index.py
@@ -1,6 +1,6 @@
 """Methods for creating an index for Oracc glossary data."""
 import elasticsearch
-from elasticsearch_dsl import analyzer, char_filter
+from elasticsearch_dsl import analyzer, char_filter, Index
 
 
 def ICU_installed(es):
@@ -9,39 +9,12 @@ def ICU_installed(es):
     return 'analysis-icu' in [p['component'] for p in cc.plugins(format="json")]
 
 
-def prepare_index_settings():
-    """Create the mapping and settings to be used for the index.
+def prepare_index_mapping():
+    """Create the mapping to be used for the index.
 
     These must be specified before ingesting the data, so that the fields are
     properly populated.
     """
-    # Some fields will contain non-ASCII characters. We want these characters
-    # to be searchable by some equivalent ASCII sequences, according to the
-    # Oracc conventions. For this, we need a custom analyzer that will replace
-    # the non-ASCII characters with their "equivalents".
-    analysis_properties = {
-        "analyzer": {
-            "cuneiform_analyzer": {
-                # The standard tokenizer will remove "," and ".", which are
-                # used in some substitution sequences; instead, we can break
-                # tokens on whitespace, which will ignore punctuation.
-                "tokenizer": "whitespace",
-                "filter": ["lowercase"],
-                "char_filter": ["cuneiform_to_ascii"]
-            }
-        },
-        # For the list of character substitutions, see
-        # http://oracc.museum.upenn.edu/doc/search/searchingcorpora/index.html#h_asciinonunicode
-        "char_filter": {
-            "cuneiform_to_ascii": {
-                "type": "mapping",
-                "mappings": [
-                    "š => sz",
-                    "ṣ => s."
-                ]
-            }
-        }
-    }
     # Create an additional field used for sorting. The new field is called
     # cf.sort and will use a locale-aware collation.
     field_properties = {
@@ -55,10 +28,8 @@ def prepare_index_settings():
             }
         }
     }
-
-    settings = {"analysis": analysis_properties}
     mappings = {"properties": field_properties}
-    return settings, mappings
+    return mappings
 
 
 def prepare_cuneiform_analyzer():
@@ -108,8 +79,8 @@ def create_index(es, index_name, type_name):
     :param index_name: the name of the index to create
     :param type_name: the name of the document type to apply the mappings to
     """
-    client = elasticsearch.client.IndicesClient(es)
-    settings, mappings = prepare_index_settings()
-    client.create(index=index_name, body={"settings": settings})
-    client.put_mapping(index=index_name, doc_type=type_name,
-                       body=mappings)
+    index = Index(index_name)
+    index.analyzer(prepare_cuneiform_analyzer())
+    index.create(using=es)
+    mappings = prepare_index_mapping()
+    index.put_mapping(using=es, doc_type=type_name, body=mappings)
