3a15ebce5149e7c2671aab04847a1166d5b75684
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2019-09-09 17:24:37 +0100

    Test search-time analysis

--- tests/test_bulk_upload.py
+++ tests/test_bulk_upload.py
@@ -1,9 +1,10 @@
 import time
 
-from elasticsearch_dsl import Index
+from elasticsearch_dsl import Index, Search
 
 import ingest.bulk_upload
-from ingest.prepare_index import ANALYZER_NAME, prepare_cuneiform_analyzer
+from ingest.prepare_index import (ANALYZER_NAME, create_index,
+                                  prepare_cuneiform_analyzer)
 
 
 def test_analyzer(es, test_index_name):
@@ -34,3 +35,17 @@ def test_upload_entries(es, entries, test_index_name):
     time.sleep(2)  # a small delay to make sure the upload has finished
     # Check that all documents have been uploaded
     assert es.count(index=test_index_name)["count"] == len(entries)
+
+
+def test_analyzer_search_results(es, entries, test_index_name):
+    """Test that the analyzer works as expected at search time."""
+    # Create the index with all the required settings
+    create_index(es, test_index_name, ingest.bulk_upload.TYPE_NAME)
+    ingest.bulk_upload.upload_entries(es, entries)
+    time.sleep(2)  # a small delay to make sure the upload has finished
+    # The test entries include "apši". Check that its ASCII transliteration
+    # can be retrieved.
+    search = Search(using=es, index=test_index_name).query("match", cf="apszi")
+    results = search.execute()
+    assert len(results) == 1  # would be 0 if no results found
+    assert results[0].cf == "apši"
