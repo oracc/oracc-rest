05f0b2cccf6c74db0783c0188ed023d66faf93e2
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2019-09-09 12:03:39 +0100

    Start testing the behaviour of the analyzer

--- tests/test_bulk_upload.py
+++ tests/test_bulk_upload.py
@@ -1,6 +1,22 @@
 import time
 
+from elasticsearch_dsl import Index
+
 import ingest.bulk_upload
+from ingest.prepare_index import ANALYZER_NAME, prepare_cuneiform_analyzer
+
+
+def test_analyzer(es, test_index_name):
+    """Test that the analyzer follows the Oracc non-ASCII representation."""
+    analyzer = prepare_cuneiform_analyzer()
+    index = Index(test_index_name)
+    index.analyzer(analyzer)
+    index.create(using=es)
+    # Check various character substitutions work
+    for (text, analyzed) in [("apši", "apszi"), ("ṣa", "s,a")]:
+        response = index.analyze(
+            using=es, body={"text": text, "analyzer": ANALYZER_NAME})
+        assert response["tokens"][0]["token"] == analyzed
 
 
 def test_upload_entries(es, entries, test_index_name):
