3e1db7a28edf7f19c90ba55034675696e9f5942e
acholyn <a.ho-lyn@ucl.ac.uk>
2023-11-22 16:23:19 +0000

    trying new log config

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from api import app
 import logging
+import sys
 
-logging.basicConfig(filename="/home/rits/logging-output.log", level=logging.DEBUG)
+# logging.basicConfig(filename="/home/rits/logging-output.log", level=logging.DEBUG)
+logging.basicConfig(
+    stream=sys.stdout,
+    filename="/home/rits/logging-output.log",
+    level=logging.DEBUG,
+    format="%(levelname)8s | [%(asctime)s] | %(name)s | %(lineno)d | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    force=True,
+)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
