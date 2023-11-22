fef0bbff2ef02309837c03527e0b6b6f0c50ad63
acholyn <a.ho-lyn@ucl.ac.uk>
2023-11-22 12:57:28 +0000

    trying different pathname

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from api import app
 import logging
 
-logging.basicConfig(filename="~/logging-output.log", level=logging.DEBUG)
+logging.basicConfig(filename="/home/logging-output.log", level=logging.DEBUG)
 
 if __name__ == "__main__":
     app.run(debug=True)
