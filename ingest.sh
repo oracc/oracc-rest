104617ef142deee93e0be48135d606e02ffff2c8
acholyn <a.ho-lyn@ucl.ac.uk>
2023-12-11 17:32:01 +0000

    adding comment about no path

--- ingest.sh
+++ ingest.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # this script is for a CRON job ingest
+# no input folder is given as it assumes oracc-rest/neo
+# if this is not correct just add a path after ingest.bulk_upload
+# eg. ingest.bulk_upload ingest/assets/dev/sample-glossaries/*
 cd /home/rits/oracc-rest
 source venv/bin/activate
 python3 -m ingest.bulk_upload
