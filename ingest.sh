f0108ccc92b03d5d3658ab89dcb09c93a46f7221
acholyn <a.ho-lyn@ucl.ac.uk>
2024-01-09 13:08:29 +0000

    updating ingest script to do logs

--- ingest.sh
+++ ingest.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 # this script is for a CRON job ingest
-# no input folder is given as it assumes oracc-rest/neo
+# no input folder is given to the python call as it assumes oracc-rest/neo
 # if this is not correct just add a path after ingest.bulk_upload
 # eg. ingest.bulk_upload ingest/assets/dev/sample-glossaries/*
+# logs will accumulate in the LOG_FILE given
+
+LOG_FILE="/home/rits/oracc-rest/ingest.log"
+
 cd /home/rits/oracc-rest
 source venv/bin/activate
-python3 -m ingest.bulk_upload
-echo "ingest complete"
+
+# Run the ingest script and capture the outcome
+python3 -m ingest.bulk_upload 2>&1 | tee -a "$LOG_FILE"
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Ingest process completed" >> "$LOG_FILE"
+
+echo "Ingest complete. See $LOG_FILE for details."
