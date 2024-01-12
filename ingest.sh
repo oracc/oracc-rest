9dffef61e3c16a7100b48c3cdeb5e40d1efa9439
acholyn <a.ho-lyn@ucl.ac.uk>
2024-01-12 11:22:55 +0000

    using systemd-cat instead

--- ingest.sh
+++ ingest.sh
@@ -3,15 +3,11 @@
 # no input folder is given to the python call as it assumes oracc-rest/neo
 # if this is not correct just add a path after ingest.bulk_upload
 # eg. ingest.bulk_upload ingest/assets/dev/sample-glossaries/*
-# logs will accumulate in the LOG_FILE given
-
-LOG_FILE="/home/rits/oracc-rest/ingest.log"
 
 cd /home/rits/oracc-rest
 source venv/bin/activate
 
-# Run the ingest script and capture the outcome
-python3 -m ingest.bulk_upload 2>&1 | tee -a "$LOG_FILE"
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Ingest process completed" >> "$LOG_FILE"
+# Run the ingest script and capture the outcome in journal
+systemd-cat -t cron-ingest python3 -m ingest.bulk_upload
 
-echo "Ingest complete. See $LOG_FILE for details."
+echo "Ingest complete. Run journalctl -t cron-ingest for logs."
